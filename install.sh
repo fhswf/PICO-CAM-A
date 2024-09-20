944a2a01badd51bb9b878e4f905b8e11b83487bb
Heiner Giefers <hgiefers@users.noreply.github.com>
2024-09-20 15:30:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sudo apt install -y git cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib
+sudo apt install -y git cmake gcc-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib g++
 cd
 git clone https://github.com/fhswf/PICO-CAM-A.git
 cd PICO-CAM-A
 https://github.com/raspberrypi/pico-sdk.git
-
+mkdir build
+cd build
+cmake -D PICO_SDK_FETCH_FROM_GIT_PATH=1 ..
+make
 
